3b84fd20c31743aa0e8a008b9ad42a7945ba96e7
jackazadian1 <44512878+jackazadian1@users.noreply.github.com>
2023-04-29 23:43:44 -0400

    logic fully developed

--- src/components/Seat/Seat.tsx
+++ src/components/Seat/Seat.tsx
@@ -1,30 +1,45 @@
 // Import React and any necessary dependencies
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Seat.module.css';
-import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 // Define a type for the component's props (if needed)
 interface SeatProps {
   seat_number: number;
   player_name: string;
-  onSeatButtonClick: (seatNumber: number) => void;
+  onSeatButtonClick: (seatNumber: number, playerName: string) => void;
 }
+
 
 // Define the PlayerHand component
 const Seat: React.FC<SeatProps> = ({ seat_number, player_name, onSeatButtonClick }) => {
+  const [playerNameInput, setPlayerNameInput] = useState('');
+
+  
   var seatStyle = styles.player_1_seat;
   if (seat_number == 2) seatStyle = styles.player_2_seat;
   else if (seat_number == 3) seatStyle = styles.player_3_seat;
   else if (seat_number == 4) seatStyle = styles.player_4_seat;
 
   return (
-    <button
-      className={`${styles.seat} ${seatStyle}`}
-      disabled={player_name != ''}
-      onClick={() => onSeatButtonClick(seat_number)}
-    >
-      {player_name == '' ? `Player ${seat_number}` : player_name}
-    </button>
+    <div className={`${styles.seat_container} ${seatStyle}`}>
+
+      {player_name == '' && 
+        <input 
+        type="text"
+        value={playerNameInput}
+        placeholder='Display Name'
+        onChange={(e) => setPlayerNameInput(e.target.value)}
+        />
+      }
+      
+      <button
+        disabled={player_name != ''}
+        onClick={() => onSeatButtonClick(seat_number, playerNameInput)}
+      >
+        {player_name == '' ? `Player ${seat_number}` : player_name}
+      </button>
+    </div>
+    
   );
 };
 
--- src/echoSetup.ts
+++ src/echoSetup.ts
@@ -3,15 +3,26 @@ import Echo from 'laravel-echo';
 // @ts-ignore
 window.Pusher = require('pusher-js');
 
-const echoInstance = new Echo({
-  broadcaster: 'pusher',
-  key: 'livepost_key', // Replace with your actual Pusher key
-  cluster: 'mt1', // Replace with your actual Pusher cluster
-  forceTLS: false,
-  wsHost: window.location.hostname,
-  wsPort: 6001,
-  encrypted: false,
-  enabledTransports: ['ws', 'wss']
-});
+const echoInstance = (room_id: string, token: string) => {
+  return new Echo({
+    broadcaster: 'pusher',
+    key: 'livepost_key', // Replace with your actual Pusher key
+    cluster: 'mt1', // Replace with your actual Pusher cluster
+    forceTLS: false,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    encrypted: false,
+    enabledTransports: ['ws', 'wss'],
+    authEndpoint: `${process.env.REACT_APP_PHP_BACKEND_API_URI}/api/broadcasting_token_auth`,
+    auth:{
+      params: {
+        room_id: room_id,
+        token: token
+      }
+    }
+  });
+}
+
+
 
 export default echoInstance;
