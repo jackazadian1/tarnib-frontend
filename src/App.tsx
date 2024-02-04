5929c32446e8d2bc53e2ed2d38d04bc2447619aa
jackazadian1 <44512878+jackazadian1@users.noreply.github.com>
2024-02-03 21:59:21 -0500

    disabled tarnib temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/room/:roomId" element={<RoomPage />} />
+          {/* <Route path="/" element={<HomePage />} />
+          <Route path="/room/:roomId" element={<RoomPage />} /> */}
           <Route path="/poker" element={<PokerHomePage />} />
           <Route path="poker/room/:roomId" element={<PokerRoomPage />} />
           <Route path="poker/room/:roomId/print" element={<PrintPage />} />
